40cba4a0a38ba8240e67f89b992e86d7344eca34
Gökay Gültekin <58526116+naphteine@users.noreply.github.com>
2023-09-15 19:00:27 +0300

    Images and web working

--- App.tsx
+++ App.tsx
@@ -1,29 +1,10 @@
 import { StatusBar } from "expo-status-bar";
-import { useState } from "react";
 import { Button, Image, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
-  const [nm, setNm] = useState(0);
-
-  const increase = () => {
-    setNm(nm + 1);
-  };
-
-  const decrease = () => {
-    setNm(nm - 1);
-  };
-
   return (
     <View style={styles.container}>
-      <Image
-        style={styles.tinyLogo}
-        source={{ uri: "https://reactnative.dev/img/tiny_logo.png" }}
-      />
-      <Text>{nm}</Text>
-      <TextInput style={styles.input}>Test</TextInput>
-      <Button title="Increase" onPress={increase}></Button>
-      <Button title="Decrease" onPress={decrease}></Button>
-
+      <Text>Hello</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -31,19 +12,9 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: "#cfcfff",
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-  },
-
-  input: {
-    backgroundColor: "gray",
-    minWidth: 320,
-    minHeight: 50,
-  },
-
-  tinyLogo: {
-    width: 50,
-    height: 50,
   },
 });
